a93aabc835da64dbbf74652c1cd42564f91e0a50
genieu99 <yujin0419@sookmyung.ac.kr>
2023-07-13 18:16:30 +0900

    [Feat] 카카오 로그인 구현(#6)

--- prProject/accounts/urls.py
+++ prProject/accounts/urls.py
@@ -1,10 +1,15 @@
+from django.contrib import admin
 from django.urls import path
 from accounts import views
+from .views import kakao_get_login, get_user_info
 
 app_name = "accounts"
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('login/kakao', kakao_get_login),
+    path('login/kakao/user/callback/', get_user_info, name="kakao_callback"),
 ]
